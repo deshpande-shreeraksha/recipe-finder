aa0f26115187797e09b60d0fc7f976ae994b4c77
deshpande-shreeraksha <deshpandeshreeraksha@gmail.com>
2025-09-25 22:30:26 +0530

    Updated

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const favoritesList = document.getElementById("favoritesList");
-  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
   if (favorites.length === 0) {
     favoritesList.innerHTML = "<p>No favorites yet. Go find something tasty!</p>";
@@ -25,5 +25,5 @@ function removeFavorite(id) {
   let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   favorites = favorites.filter(recipe => recipe.id !== id);
   localStorage.setItem("favorites", JSON.stringify(favorites));
-  location.reload(); // refresh list
+  location.reload();
 }
